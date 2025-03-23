3c52167dfaea2008d527d585695e73a8b66b85a9
UrviJoshi24 <urvigithub334@gmail.com>
2025-03-23 15:49:38 +0530

    mediadata.js updated final

--- my-app/src/components/mediaData.js
+++ my-app/src/components/mediaData.js
@@ -1,192 +1,56 @@
 import bicycleVideo from '../assets/videos/bicycle-friends-happy.mp4';
 import enterHappyVideo from '../assets/videos/enter-happy.mp4';
 import familyHappyVideo from '../assets/videos/family-happy.mp4';
-import fireworksHappyVideo from '../assets/videos/fireworks-happy.mp4';
+import childHappyVideo from '../assets/videos/child-happy.mp4';
 import girlNatureHappyVideo from '../assets/videos/girl-nature-happy.mp4';
 import holidaysHappyVideo from '../assets/videos/holidays-happy.mp4';
 import meetFriendHappyVideo from '../assets/videos/meet-friend-happy.mp4';
 import natureHappyVideo from '../assets/videos/nature-happy.mp4';
 import personDogHappyVideo from '../assets/videos/person-dog-happy.mp4';
 import studentsVideo from '../assets/videos/students-happy.mp4';
+import blamingSad from '../assets/videos/blaming-sad.mp4';
+import couplefightSad from '../assets/videos/couple-fight-sad.mp4';
+import deathSad from '../assets/videos/death-sad.mp4';
+import failedSad from '../assets/videos/failed-sad.mp4';
+import financialSad from '../assets/videos/financial-sad.mp4';
+import friendsarugSad from '../assets/videos/friends-arug-sad.mp4';
+import layoffSad from '../assets/videos/layoff-sad.mp4';
+import teacheryellingSad from '../assets/videos/teacher-yelling-sad.mp4';
+import parentsFightSad from '../assets/videos/parents-sad.mp4';
+import brokenDownSad from '../assets/videos/brokendown-sad.mp4';
+import stressSad from '../assets/videos/stress-sad.mp4';
+import aloneSad from '../assets/videos/alone-sad.mp4';
+import foodHappy from '../assets/videos/food-happy.mp4';
+import peacefully from '../assets/videos/peacefully-happy.mp4';
+
 export const media = {
   Happy: [
-    {
-      id: 1,
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDLqNtZw0QdFpXxJJbgM_iwygQ3DHVJNEEUQ&s',
-      type: 'image',
-      description: 'Cute Puppy',
-      question: 'Would you like to cuddle this adorable puppy?',
-    },
-    {
-      id: 2,
-      src: 'https://img.freepik.com/premium-photo/two-indian-school-friends-smiling-together-school_130568-366.jpg',
-      type: 'image',
-      description: 'Smiling Friends',
-      question: 'Do you have a favorite memory with your childhood friends?',
-    },
-    {
-      id: 3,
-      src: 'https://media.istockphoto.com/id/1700386148/video/child-tickling-sibling-on-the-beach-on-the-feet-with-feather-kid-cover-in-sand-smiling.mp4?s=mp4-640x640-is&k=20&c=CUoherEQ9uJwpiCsL-tQaneursQCNVWjIFfU-Q5YPUA=',
-      type: 'video',
-      description: 'Happy Kids Playing (Online)',
-      question: 'When was the last time you played like a carefree child?',
-    },
-    {
-      id: 4,
-      src: bicycleVideo,
-      type: 'video',
-      description: 'Cycling Fun',
-      question: 'When was the last time you rode a bicycle for fun?',
-    },
-    {
-      id: 5,
-      src: enterHappyVideo,
-      type: 'video',
-      description: 'Excited Entry',
-      question: 'Have you ever felt this excited entering a place?',
-    },
-    {
-      id: 6,
-      src: familyHappyVideo,
-      type: 'video',
-      description: 'Family Gathering',
-      question: 'What’s your favorite family gathering memory?',
-    },
-    {
-      id: 7,
-      src: fireworksHappyVideo,
-      type: 'video',
-      description: 'Fireworks Celebration',
-      question: 'Do fireworks bring back special celebration memories for you?',
-    },
-    {
-      id: 8,
-      src: girlNatureHappyVideo,
-      type: 'video',
-      description: 'Girl Enjoying Nature',
-      question: 'When was the last time you felt at peace in nature?',
-    },
-    {
-      id: 9,
-      src: holidaysHappyVideo,
-      type: 'video',
-      description: 'Holiday Fun',
-      question: 'What’s your happiest holiday memory?',
-    },
-    {
-      id: 10,
-      src: meetFriendHappyVideo,
-      type: 'video',
-      description: 'Meeting a Friend',
-      question: 'How did you feel the last time you reunited with an old friend?',
-    },
-    {
-      id: 11,
-      src: natureHappyVideo,
-      type: 'video',
-      description: 'Peaceful Nature',
-      question: 'Does spending time in nature bring you happiness?',
-    },
-    {
-      id: 12,
-      src: personDogHappyVideo,
-      type: 'video',
-      description: 'Person with Dog',
-      question: 'Do you share a special bond with a pet like this?',
-    },
-    {
-      id: 13,
-      src: studentsVideo,
-      type: 'video',
-      description: 'Students Celebrating',
-      question: 'What’s your most joyful moment as a student?',
-    },
+    { id: 1, src: childHappyVideo, type: 'video', description: 'Happy Kids Playing', question: 'When was the last time you played like a carefree child?' },
+    { id: 2, src: bicycleVideo, type: 'video', description: 'Cycling Fun', question: 'When was the last time you rode a bicycle for fun?' },
+    { id: 3, src: enterHappyVideo, type: 'video', description: 'Excited Entry', question: 'Have you ever felt this excited entering a place?' },
+    { id: 4, src: familyHappyVideo, type: 'video', description: 'Family Gathering', question: "What's your favorite family gathering memory?" },
+    { id: 5, src: girlNatureHappyVideo, type: 'video', description: 'Girl Enjoying Nature', question: 'When was the last time you felt at peace in nature?' },
+    { id: 6, src: holidaysHappyVideo, type: 'video', description: 'Holiday Fun', question: "What's your happiest holiday memory?" },
+    { id: 7, src: meetFriendHappyVideo, type: 'video', description: 'Meeting a Friend', question: 'How did you feel the last time you reunited with an old friend?' },
+    { id: 8, src: natureHappyVideo, type: 'video', description: 'Peaceful Nature', question: 'Does spending time in nature bring you happiness?' },
+    { id: 9, src: personDogHappyVideo, type: 'video', description: 'Person with Dog', question: 'Do you share a special bond with a pet like this?' },
+    { id: 10, src: studentsVideo, type: 'video', description: 'Students Celebrating', question: "What's your most joyful moment as a student?" },
+    { id: 11, src: foodHappy, type: 'video', description: 'Happily cooking food for yourself like him', question: "What’s your favorite meal to cook for yourself?" },
+    { id: 12, src: peacefully, type: 'video', description: 'Reading book peacefully in library', question: "When was the last time you lost yourself in a great book?" },
   ],
 
   Sad: [
-    {
-      id: 15,
-      src: 'https://t3.ftcdn.net/jpg/00/65/39/74/360_F_65397486_1brvJ9KipdByBoFpNOLpIJxFdwSigXIY.jpg',
-      type: 'image',
-      description: 'Plain Landscape',
-      question: 'Does this calm or bore you?',
-    },
-    {
-      id: 16,
-      src: 'https://images.pexels.com/photos/5669338/pexels-photo-5669338.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-      type: 'image',
-      description: 'Calm Lake',
-      question: 'Would you like to sit here and reflect?',
-    },
-    {
-      id: 17,
-      src: 'https://dy7glz37jgl0b.cloudfront.net/advice/images/122b354d5e0dd05b511b34a2a19ed840-black-woman-gazing-out-window-couch_l.jpeg',
-      type: 'image',
-      description: 'Lonely Person',
-      question: 'Have you ever felt this lonely?',
-    },
-    {
-      id: 18,
-      src: 'https://media.istockphoto.com/id/924201818/video/upset-male-child-suffering-from-parent-quarrels-depression-domestic-violence.mp4?s=mp4-640x640-is&k=20&c=wL043c-nuer1rRoTX4M-8yDTEdC_3rWw5WCf2bXwtmE=',
-      type: 'video',
-      description: 'Parents fighting',
-      question: 'Do conflicts at home affect your mood?',
-    },
-    {
-      id: 19,
-      src: 'https://media.istockphoto.com/id/1993516553/video/one-male-person-covering-face-in-shame-sitting-at-home-floor-in-the-dark-during-crisis-person.mp4?s=mp4-640x640-is&k=20&c=wGkQ_IBGkpWb4T88hfIHRgrwTtDrvdOBKcyRmolL-cE=',
-      type: 'video',
-      description: 'Sad Woman Sitting Alone',
-      question: 'Have you ever felt overwhelmed like this?',
-    },
-    {
-      id: 20,
-      src: 'https://www.shutterstock.com/shutterstock/videos/3719371745/thumb/11.jpg?ip=x480',
-      type: 'image',
-      description: 'Fight against friends',
-      question: 'Have you ever argued with a close friend?',
-    },
-    {
-      id: 21,
-      src: 'https://thumbs.dreamstime.com/b/teacher-scolding-student-girl-rampage-angry-blame-her-92396982.jpg',
-      type: 'image',
-      description: 'Angry Student at Teacher',
-      question: 'Have you ever felt misunderstood by a teacher?',
-    },
-    {
-      id: 22,
-      src: 'https://media.istockphoto.com/id/1191132580/photo/frustrated-millennial-female-worker-felling-tired-of-working-quarreling.jpg?s=612x612&w=0&k=20&c=Y6oaOPB7WaGe3WlwCrChzGKndU3dGPvzouUrO1N265I=',
-      type: 'image',
-      description: 'Conflict Scene',
-      question: 'How do you handle conflict at work or school?',
-    },
-    {
-      id: 23,
-      src: 'https://www.shutterstock.com/shutterstock/videos/1055967437/preview/stock-footage-a-frustrated-angry-dominating-male-boss-businessman-shouting-at-an-office-corporate-female.webm',
-      type: 'video',
-      description: 'Angry Boss on Employee',
-      question: 'Have you been yelled at in public?',
-    },
-    {
-      id: 24,
-      src: 'https://static.vecteezy.com/system/resources/thumbnails/049/532/277/large/devil-vampire-portrait-free-video.jpg',
-      type: 'image',
-      description: 'Dark Forest',
-      question: 'Does darkness scare you?',
-    },
-    {
-      id: 25,
-      src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN4kh3hFtcagyMNyNWsSKGmYMj4ekI4aPUVA&s',
-      type: 'image',
-      description: 'Shadowy Figure',
-      question: 'Have you ever felt someone was watching you?',
-    },
-    {
-      id: 26,
-      src: 'https://www.lifeofpretty.com/wp-content/uploads/2021/11/Picture3-2.jpg',
-      type: 'image',
-      description: 'Scary Shadow Moving',
-      question: 'Does this image make you uneasy?',
-    },
+    { id: 13, src: parentsFightSad, type: 'video', description: 'Parents fighting', question: 'Do conflicts at home affect your mood?' },
+    { id: 14, src: brokenDownSad, type: 'video', description: 'Broken down in tears', question: 'Have you ever felt overwhelmed like this?' },
+    { id: 15, src: aloneSad, type: 'video', description: 'Felt alone in public or in parties', question: 'Have you ever felt lonely in a crowd? How did you handle it?' },
+    { id: 16, src: blamingSad, type: 'video', description: 'Someone is blaming at you', question: 'How do you react when you’re wrongly accused of something?' },
+    { id: 17, src: couplefightSad, type: 'video', description: 'Fight in between couples', question: 'What do you think is the key to resolving conflicts in a relationship?' },
+    { id: 18, src: deathSad, type: 'video', description: 'Death of your loved one', question: 'How did you cope with losing someone dear to you?' },
+    { id: 19, src: failedSad, type: 'video', description: 'Failed in exams after doing hard work', question: 'How do you handle disappointment after putting in so much effort?' },
+    { id: 20, src: financialSad, type: 'video', description: 'Facing problem for financials', question: 'What financial challenges have you faced, and how did you overcome them?' },
+    { id: 21, src: friendsarugSad, type: 'video', description: 'Friends arguing with you', question: 'How do you mend a friendship after a serious argument?' },
+    { id: 22, src: layoffSad, type: 'video', description: 'Layoff from your job without any mistake', question: 'How do you deal with unexpected setbacks in your career?' },
+    { id: 23, src: teacheryellingSad, type: 'video', description: 'Teacher is yelling at you in front of the whole class', question: 'How did you feel when you were scolded publicly? How did you respond?' },
+    { id: 24, src: stressSad, type: 'video', description: 'Tensed about anything throughout the day', question: 'Have you felt tensed throughtout the day?' },
   ],
 };
